3da8ac9d31e1b05c542b5318e4aa01e95305eaf8
KickStartDevOps100 <149104588+KickStartDevOps100@users.noreply.github.com>
2023-10-31 21:11:09 +0000

    Update Finance.java
    Changed in GitHub

--- Finance/src/com/vel/Finance.java
+++ Finance/src/com/vel/Finance.java
@@ -14,6 +14,7 @@ public class Finance {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Eclipse/GitHub!!!!");
+		System.out.println("Changed In GitHub!!!!");
 
 	}
 
