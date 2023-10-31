220ea5c6ad22fc4cf14e0be0cb74e6c7ca2f3046
velmu <velmu@192.168.0.4>
2023-10-31 18:00:40 +0000

    New commit

---
+++ Finance/src/com/vel/Finance.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.vel;
+
+/**
+ * 
+ */
+public class Finance {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Eclipse/GitHub!!!");
+
+	}
+
+}
