a5af8f212fc24d9390c55cf5898d6566acd64176
velmu <velmu@DESKTOP-6HAMS6L.Home>
2023-10-31 21:22:42 +0000

    System.out.println("Changed In Eclipse!!!!");

--- FinanceMaven/FinanceMaven/src/main/java/com/vel/FinanceMaven/Welcome.java
+++ FinanceMaven/FinanceMaven/src/main/java/com/vel/FinanceMaven/Welcome.java
@@ -5,6 +5,7 @@ public class Welcome {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Maven ---- Eclipse/GitHub!!!!!!");
+		System.out.println("Changed In Eclipse!!!!");	
 	}
 
 }
