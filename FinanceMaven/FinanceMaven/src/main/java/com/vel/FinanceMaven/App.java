5aceda1dad51ccff6da49bcbdf1cf5cc4e9f61fe
velmu <velmu@DESKTOP-6HAMS6L.Home>
2023-10-31 20:37:01 +0000

    first commit

---
+++ FinanceMaven/FinanceMaven/src/main/java/com/vel/FinanceMaven/App.java
@@ -0,0 +1,13 @@
+package com.vel.FinanceMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
